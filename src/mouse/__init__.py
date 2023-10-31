a43aecb8eb1c14324dda29d98542984d8339a047
Ian Santopietro <isantop@gmail.com>
2023-10-31 13:41:10 -0600

    feat: make basic functioning GTK app

--- src/mouse/__init__.py
+++ src/mouse/__init__.py
@@ -8,3 +8,29 @@ file, You can obtain one at http://mozilla.org/MPL/2.0/.
 Mouse - The anony-mouse, privacy focused browser view.
 """
 
+import gi
+
+gi.require_versions({
+    'Gtk': '4.0',
+    'Gio': '2.0',
+})
+
+from .gui.window import MouseWindow
+from gi.repository import Gtk, Gio
+
+VERSION = '0.0.1'
+
+def make_window(app, data=None):
+    # window = MouseWindow.new(app)
+    window = Gtk.ApplicationWindow.new(app)
+    window.present()
+
+def run_mouse():
+    app = Gtk.Application.new(
+        'ro.santopiet.mouse', 
+        Gio.ApplicationFlags.DEFAULT_FLAGS
+    )
+
+    app.connect('activate', make_window)
+
+    app.run(None)
